dcc58ea3d8ab4e042c0ed66bba84219b1b74da16
Martin Barri <maeertin@gmail.com>
2015-09-27 22:32:47 +0200

    Updated brew cask app list

--- brew-cask.sh
+++ brew-cask.sh
@@ -11,39 +11,36 @@ brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 # daily
-brew cask install spectacle
+brew cask install notational-velocity
+brew cask install the-unarchiver
+brew cask install scroll-reverser
+brew cask install totalfinder
+brew cask install clipmenu
 brew cask install dropbox
-brew cask install gyazo
-brew cask install onepassword
-brew cask install rescuetime
+brew cask install spotify
+brew cask install noizio
+brew cask install skype
+brew cask install slack
 brew cask install flux
 
 # dev
-brew cask install iterm2
 brew cask install sublime-text
 brew cask install imagealpha
 brew cask install imageoptim
-
-# fun
-brew cask install limechat
-brew cask install miro-video-converter
-brew cask install horndis               # usb tethering
+brew cask install sequel-pro
+brew cask install cyberduck
+brew cask install iterm2
 
 # browsers
-brew cask install google-chrome-canary
-brew cask install firefox-nightly
-brew cask install webkit-nightly
-brew cask install chromium
-brew cask install torbrowser
+brew cask install google-chrome
+brew cask install firefox
 
-# less often
+# fun
+brew cask install miro-video-converter
 brew cask install disk-inventory-x
-brew cask install screenflow4 # 4 specifically not 5.
+brew cask install transmission
+brew cask install gyazo
 brew cask install vlc
-brew cask install gpgtools
-brew cask install licecap
-brew cask install utorrent
-
 
 # Not on cask but I want regardless.
-# 
+# Mullvad
