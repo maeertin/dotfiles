5ef29949fd7af32360686c880fd6a746fd82e2da
Martin Barri <maeertin@gmail.com>
2015-10-05 01:37:59 +0200

    Fixed dropbox symlink paths

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -89,7 +89,6 @@ fi
 
 
 
-
 ##############################################################################################################
 ### initial setup
 
@@ -101,7 +100,6 @@ mkdir ~/Sites/github
 
 ### end of initial setup
 ##############################################################################################################
-
 
 
 
@@ -117,70 +115,6 @@ export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
 ./brew-cask.sh
 
 ### end of homebrew
-##############################################################################################################
-
-
-
-
-##############################################################################################################
-### install of common things
-###
-
-# github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
-
-# Type `git open` to open the GitHub page or website for a repository.
-npm install -g git-open
-
-
-# github.com/rupa/z   - oh how i love you
-git clone https://github.com/rupa/z.git ~/code/z
-chmod +x ~/code/z/z.sh
-# consider reusing your current .z file if possible. it's painful to rebuild :)
-# z is hooked up in .bash_profile
-
-
-# github.com/thebitguru/play-button-itunes-patch
-# disable itunes opening on media keys
-git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
-
-
-# my magic photobooth symlink -> dropbox. I love it.
-# 	 + first move Photo Booth folder out of Pictures
-# 	 + then start Photo Booth. It'll ask where to put the library.
-# 	 + put it in Dropbox/public
-# 	* Now… you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# 	* then you grab public URL and send off your video message in a heartbeat.
-
-
-# for the c alias (syntax highlighted cat)
-sudo easy_install Pygments
-
-
-# change to bash 4 (installed by homebrew)
-BASHPATH=$(brew --prefix)/bin/bash
-sudo echo $BASHPATH >> /etc/shells
-chsh -s $BASHPATH # will set for current user only.
-echo $BASH_VERSION # should be 4.x not the old 3.2.X
-# Later, confirm iterm settings aren't conflicting.
-
-
-# iterm with more margin! http://hackr.it/articles/prettier-gutter-in-iterm-2/
-#   (admittedly not as easy to maintain)
-
-
-# setting up the sublime symlink
-ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
-
-
-# setting up the sublime packages symlink
-rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
-ln -sf ~/Dropbox/maeertin\ settings/Sublime\ 3/Packages ~/Library/Application\ Support/Sublime\ Text\ 3/
-
-
-###
 ##############################################################################################################
 
 
@@ -216,8 +150,70 @@ sh .osx
 ./symlink-setup.sh
 
 # setting up the .extra symlink
-ln -sf "~/Dropbox/maeertin\ settings/.extra" ~/
+ln -sf ~/Dropbox/maeertin\ settings/.extra ~/
 
 ###
 ##############################################################################################################
 
+
+
+##############################################################################################################
+### install of common things
+###
+
+# github.com/jamiew/git-friendly
+# the `push` command which copies the github compare URL to my clipboard is heaven
+bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+
+
+# Type `git open` to open the GitHub page or website for a repository.
+npm install -g git-open
+
+
+# github.com/rupa/z   - oh how i love you
+git clone https://github.com/rupa/z.git ~/code/z
+chmod +x ~/code/z/z.sh
+# consider reusing your current .z file if possible. it's painful to rebuild :)
+# z is hooked up in .bash_profile
+
+
+# github.com/thebitguru/play-button-itunes-patch
+# disable itunes opening on media keys
+git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
+
+
+# my magic photobooth symlink -> dropbox. I love it.
+#    + first move Photo Booth folder out of Pictures
+#    + then start Photo Booth. It'll ask where to put the library.
+#    + put it in Dropbox/public
+#   * Now… you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
+#   * then you grab public URL and send off your video message in a heartbeat.
+
+
+# for the c alias (syntax highlighted cat)
+sudo easy_install Pygments
+
+
+# change to bash 4 (installed by homebrew)
+BASHPATH=$(brew --prefix)/bin/bash
+sudo echo $BASHPATH >> /etc/shells
+chsh -s $BASHPATH # will set for current user only.
+echo $BASH_VERSION # should be 4.x not the old 3.2.X
+# Later, confirm iterm settings aren't conflicting.
+
+
+# iterm with more margin! http://hackr.it/articles/prettier-gutter-in-iterm-2/
+#   (admittedly not as easy to maintain)
+
+
+# setting up the sublime symlink
+ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+
+
+# setting up the sublime packages symlink
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+ln -sf ~/Dropbox/maeertin\ settings/Sublime\ 3/Packages ~/Library/Application\ Support/Sublime\ Text\ 3/
+
+
+###
+##############################################################################################################
