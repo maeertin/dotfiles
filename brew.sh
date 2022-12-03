07ac8c42d31949bdd0c7609999e35d51c11801e2
Martin Barri <maeertin@gmail.com>
2022-12-03 21:03:24 +0100

    remove unused binarys and add new

--- brew.sh
+++ brew.sh
@@ -5,12 +5,6 @@
 # (Optionally) Turn off brew's analytics https://docs.brew.sh/Analytics
 # brew analytics off
 
-# Make sure we’re using the latest Homebrew
-brew update
-
-# Upgrade any already-installed formulae
-brew upgrade
-
 
 # GNU core utilities (those that come with OS X are outdated)
 brew install coreutils
@@ -23,10 +17,9 @@ brew install gnu-sed
 
 # Updated shells
 # Note: don’t forget to add `/usr/local/bin/<EACHSHELL>` to `/etc/shells` before running `chsh`.
-brew install bash
-brew install zsh
+brew install fish
 
-brew install bash-completion@2
+brew install bash-completion
 
 
 # Install wget
@@ -36,26 +29,17 @@ brew install wget
 brew install vim
 brew install nano
 brew install grep
-brew install openssh
 
 # z hopping around folders
 brew install z
 
-# github util
+# git utils
 brew install gh
-# nicer git diffs
-brew install git-delta
 brew install git-friendly/git-friendly/git-friendly
-brew install git
-
-# better `top`
-brew install glances
 
 # Install other useful binaries
-brew install ffmpeg
-brew install ncdu # find where your diskspace went
 brew install node # This installs `npm` too using the recommended installation method
 brew install tree # present the content of a given directory
-
-# Remove outdated versions from the cellar
-brew cleanup
+brew install neovim
+brew install ripgrep
+brew install ffmpeg
