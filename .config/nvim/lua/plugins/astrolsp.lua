8340509ecb4ff962b140c7e1bab33004ab513b8e
Martin Barri <maeertin@gmail.com>
2024-08-06 10:20:59 +0200

    comment out astrolsp.lua config

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -1,23 +1,25 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
 --       as this provides autocomplete and documentation while editing
 
----@type LazySpec
-return {
-  "AstroNvim/astrolsp",
-  ---@type AstroLSPOpts
-  opts = {
-    -- Configure default capabilities for language servers (`:h vim.lsp.protocol.make_client.capabilities()`)
-    capabilities = {
-      textDocument = {
-        foldingRange = {
-          -- BUG: RPC[Error] code_name = InternalError, message = "Request textDocument/foldingRange failed ..."
-          -- @see https://github.com/redhat-developer/yaml-language-server/issues/912
-          dynamicRegistration = false,
-          lineFoldingOnly = true,
-        },
-      },
-    },
-  },
-}
+-- ---@type LazySpec
+-- return {
+--   "AstroNvim/astrolsp",
+--   ---@type AstroLSPOpts
+--   opts = {
+--     -- Configure default capabilities for language servers (`:h vim.lsp.protocol.make_client.capabilities()`)
+--     capabilities = {
+--       textDocument = {
+--         foldingRange = {
+--           -- BUG: RPC[Error] code_name = InternalError, message = "Request textDocument/foldingRange failed ..."
+--           -- @see https://github.com/redhat-developer/yaml-language-server/issues/912
+--           -- dynamicRegistration = false,
+--           -- lineFoldingOnly = true,
+--         },
+--       },
+--     },
+--   },
+-- }
